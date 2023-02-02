52fe3f04705b42f4085d607d84e4e930368a93c9
Martina2911 <104918343+Martina2911@users.noreply.github.com>
2023-02-02 20:22:20 +0200

    Update GraphAnalysis.java

--- Project DS/src/xmlObjects/GraphAnalysis.java
+++ Project DS/src/xmlObjects/GraphAnalysis.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @author HP
  */
 public class GraphAnalysis {
-    
+    //returning the ids of the most followed users
     public static  ArrayList<Integer> Mostactive(int[][] matrix){
         ArrayList<Integer> result = new ArrayList<Integer>();
         int[] count = new int[matrix.length];
@@ -34,6 +34,40 @@ public class GraphAnalysis {
         return result;
     }
     
+    // returning ids of users following the most
+    public static  ArrayList<Integer> Mostactive(int[][] matrix){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int[] count = new int[matrix.length];
+        for(int i =0;i<matrix.length;i++){
+          for(int j =0;j<matrix[0].length;j++){  
+              if(matrix[i][j] == 1)
+                 count[i] += 1;
+          }
+        }
+        int indexMost = 0;
+        for(int z =1;z<count.length;z++){
+            if(count[z] > count[indexMost])
+               indexMost = z;
+        }
+        for(int z =0;z<count.length;z++){
+            if(count[indexMost] == count[z])
+               result.add(z+1);
+        }
+        return result;
+    }
+    
+    //returning the ids of mutual users between any 2 users 
+     public static  ArrayList<Integer> MutualFollowers(int[][] matrix,int id1,int id2){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i =0; i<matrix.length;i++){
+            if(matrix[id1-1][i] == 1 && matrix[id1-1][i]== matrix[id2-1][i] ){
+                result.add(i+1);
+            }  
+        }
+        return result;
+    }
+
+    //returning the ids of recommendations for a certain user
     public static ArrayList<Integer> suggest(int[][] matrix , int id){
         ArrayList<Integer> list = new ArrayList<Integer>();
         ArrayList<Integer> MyFollowers = new ArrayList<Integer>();
