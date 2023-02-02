02c4c066582080603630fe5702143b3e422b8141
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2023-02-02 19:00:23 +0200

    add postSearch

--- Project DS/src/xmlObjects/Graph.java
+++ Project DS/src/xmlObjects/Graph.java
@@ -1,40 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package xmlObjects;
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-/**
- *
- * @author Youssef
- */
 public class Graph {
-    
-    HashMap <Integer,Integer> m = new HashMap<>();
-    XMLUsers users ;
-    
-    int[][] array = new int[users.users.size()][users.users.size()];
-    
-    
-    
-    
+    HashMap <Integer,Integer> m;
+    XMLUsers users;
+    int[][] adjMat;
+
     public Graph(XMLUsers users) {
         this.users = users;
-        
-        
+        adjMat = new int[users.len()][users.len()];
+        m = new HashMap<>();
+        mapCreator();
     }
-    
-    
-    
+
     void addEdge(int x, int y){
-        
-        array [x][y] = 1; 
-        
+        adjMat [x][y] = 1;
     }
     
     void arrayMaker (ArrayList<User> users){
@@ -43,30 +25,108 @@ public class Graph {
             
             for(int j=0; j<users.get(i).getFollowers().size();j++){
                 
-                array [users.get(i).getId()] [users.get(i).getFollowers().get(j).getId()]=1;
+                adjMat [m.get(users.get(i).getId())] [m.get(users.get(i).getFollowers().get(j).getId())]=1;
                 
             }
-            
         }
-        
     } 
    
-    HashMap GraphCreator(ArrayList<User> users){
-        
-       
-        
-        for(int i=0; i<users.size();i++){
-            
-            m.put(users.get(i).getId(),i );
-            
+    void mapCreator(){
+        for(int i=0; i<users.len();i++){
+            this.m.put(users.getByIndex(i).getId(), i);
         }
-        
-      return m;
-        
     }
-    
-    
-    
-    
-    
+
+    ArrayList<Post> postSearch(String searchWords){
+        String[] searchArray = searchWords.split("[^\\w']+");
+
+        ArrayList<Post> pf = new ArrayList<>();
+        int uSize = this.users.len();
+
+        // iterate for each user
+        for(int i=0; i<uSize; i++){
+            User user = users.getByIndex(i);
+            int pSize = user.getPosts().size();
+
+            //iterate for each post
+            for(int j=0; j<pSize;j++){
+                Post post = user.getPosts().get(j);
+                String[] bodyWords = post.getBody().split("[^\\w']+"); // split post into array of words
+
+                //iterate for each word
+                for(int k=0; k< bodyWords.length; k++){
+                    boolean matched = false;
+
+                   for(int h=0; h<searchArray.length; h++){
+                       if(searchArray[h].equalsIgnoreCase(bodyWords[k+h])){
+                           matched = true;
+                       }
+                       else{
+                           matched = false;
+                           break;
+                       }
+                   }
+                   if(matched == true)
+                       pf.add(post);
+                }
+            }
+        }
+        return pf;
+    }
+
+    /*
+    static ArrayList<Post> Search(String searchWords, ArrayList<Post> postat){
+        String[] searchArray = searchWords.split("[^\\w']+");
+
+        ArrayList<Post> pf = new ArrayList<>();
+
+
+            int pSize = postat.size();
+
+            //iterate for each post
+            for(int j=0; j<pSize;j++){
+                Post post = postat.get(j);
+                String[] bodyWords = post.getBody().split("[^\\w']+"); // split post into array of words
+
+                //iterate for each word
+                for(int k=0; k< bodyWords.length; k++){
+                    boolean matched = false;
+
+                    for(int h=0; h<searchArray.length; h++){
+                        if(searchArray[h].equalsIgnoreCase(bodyWords[k+h])){
+                            matched = true;
+                        }
+                        else{
+                            matched = false;
+                            break;
+                        }
+                    }
+                    if(matched == true)
+                        pf.add(post);
+                }
+            }
+        return pf;
+    }
+    */
+
+    public static void main(String[] args) {
+
+        /*
+        Post p1 = new Post();
+        p1.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+        Post p2 = new Post();
+        p2.setBody("Oneday i was young");
+        Post p3 = new Post();
+        p3.setBody("OneDay you will be older");
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(p1);
+        posts.add(p2);
+        posts.add(p3);
+        ArrayList<Post> sp = Search("OneDay you will be", posts);
+        for (Post p: sp){
+            System.out.println(p.getBody());
+        }
+        */
+
+    }
 }
--- Project DS/src/xmlObjects/XMLUsers.java
+++ Project DS/src/xmlObjects/XMLUsers.java
@@ -8,19 +8,25 @@ public class XMLUsers{
     public ArrayList<User> getUsers() { 
          return this.users; 
     }
+
     public void setUsers(ArrayList<User> users) { 
          this.users = users;
     }
     
     public int getid(User u){
-        
         return u.getId();
-        
     }
     
     public ArrayList<Follower> getFollowers(User u){
-        
         return u.getFollowers();
+    }
+
+    public int len(){
+        return users.size();
+    }
+
+    public User getByIndex(int i){
+        return users.get(i);
     }
     
 }
